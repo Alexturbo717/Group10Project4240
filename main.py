c58ac25140589dee5214b4cdc5a702c025f2d8d3
Abdul-Hadi Hashmi <hadihashmi808@gmail.com>
2026-04-09 14:08:40 -0500

    add notes to the file for better understanding

--- main.py
+++ main.py
@@ -3,31 +3,34 @@
 import cv2
 def main():
 
+    # Load the Haar Cascade for face detection
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     
+    # Start video capture from the webcam
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print("Error: Could not open webcam.")
         return
     
+    # Read frames from the webcam and perform face detection
     while True:
         ret, frame = cap.read()
         if not ret:
             print("Error: Could not read frame.")
             break
         
+        # Convert the frame to grayscale for face detection
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
        
+       # Draw rectangles around detected faces
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        
         cv2.imshow('Face Detection', frame)
-        
+       
+        # Release the video capture and close windows
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    
     cap.release()
     cv2.destroyAllWindows()
 
